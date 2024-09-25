850645f3def2fb8c1257a7e84c0bcac328f61b50
Santo1997 <hossainsanto2@gmail.com>
2024-09-25 21:05:03 +0600

    admin controller set

---
+++ resources/views/helper/notify.blade.php
@@ -0,0 +1,9 @@
+<section class="relative">
+    <progress id="loader" class="progress progress-info absolute -bottom-2 left-0 right-0 z-10 hidden"></progress>
+
+    <div id="toast" class="toast toast-top z-50 hidden">
+        <div id="toast-icon" class="alert">
+            <span id="toast-msg" class="text-white"></span>
+        </div>
+    </div>
+</section>
